0efce23b7f5ba823271a3596e1d39c2f640a4f2a
Hafidz Athaya <mhafidza528@gmail.com>
2025-10-05 20:19:42 +0800

    Implement RegisterNewUser and GetUserByEmail service method

---
+++ internal/service/user_service_impl.go
@@ -0,0 +1,89 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/mhaatha/go-template-saygenfix/internal/helper"
+	"github.com/mhaatha/go-template-saygenfix/internal/model/domain"
+	"github.com/mhaatha/go-template-saygenfix/internal/model/web"
+	"github.com/mhaatha/go-template-saygenfix/internal/repository"
+)
+
+func NewUserService(userRepository repository.UserRepository, db *pgxpool.Pool, validate *validator.Validate) UserService {
+	return &UserServiceImpl{
+		UserRepository: userRepository,
+		DB:             db,
+		Validate:       validate,
+	}
+}
+
+type UserServiceImpl struct {
+	UserRepository repository.UserRepository
+	DB             *pgxpool.Pool
+	Validate       *validator.Validate
+}
+
+func (service *UserServiceImpl) RegisterNewUser(ctx context.Context, request web.RegisterUserRequest) error {
+	// Validate request
+	err := service.Validate.Struct(request)
+	if err != nil {
+		return err
+	}
+
+	// Open transaction
+	tx, err := service.DB.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer helper.CommitOrRollback(ctx, tx)
+
+	// Check if email already exists
+	existingUser, err := service.UserRepository.FindByEmail(ctx, tx, request.Email)
+	if err != nil {
+		return err
+	}
+	if existingUser.Id != "" {
+		return errors.New("email already exists")
+	}
+
+	// Hash password
+	hashedPassword, err := helper.HashPassword(request.Password)
+	if err != nil {
+		return err
+	}
+
+	user := domain.User{
+		Email:    request.Email,
+		FullName: request.FullName,
+		Password: hashedPassword,
+		Role:     request.Role,
+	}
+
+	return service.UserRepository.Save(ctx, tx, user)
+}
+
+func (service *UserServiceImpl) GetUserByEmail(ctx context.Context, email string) (domain.User, error) {
+	// Open transaction
+	tx, err := service.DB.Begin(ctx)
+	if err != nil {
+		return domain.User{}, err
+	}
+	defer helper.CommitOrRollback(ctx, tx)
+
+	// Check is email exists
+	user, err := service.UserRepository.FindByEmail(ctx, tx, email)
+	if err != nil {
+		return domain.User{}, err
+	}
+	if user.Id == "" {
+		return domain.User{}, errors.New("user not found")
+	}
+
+	fmt.Println(user)
+
+	return user, nil
+}
