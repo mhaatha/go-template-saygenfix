29e871eac22cea215d6776d5acbe3e23713e7cab
Hafidz Athaya <mhafidza528@gmail.com>
2025-10-05 20:11:09 +0800

    Environment config

---
+++ internal/config/env.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	AppPort string
+	DBURL   string
+
+	SessionName   string
+	SessionMaxAge string
+}
+
+func LoadConfig() (*Config, error) {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		return &Config{}, err
+	}
+
+	return &Config{
+		AppPort: os.Getenv("APP_PORT"),
+		DBURL:   os.Getenv("DB_URL"),
+
+		SessionName:   os.Getenv("SESSION_NAME"),
+		SessionMaxAge: os.Getenv("SESSION_MAX_AGE"),
+	}, nil
+}
