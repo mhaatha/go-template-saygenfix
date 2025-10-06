ac987104be572724098c26840597bc8cfbd989fb
Hafidz Athaya <mhafidza528@gmail.com>
2025-10-06 10:42:35 +0800

    Add /room-ujian-teacher route

---
+++ internal/router/teacher_router.go
@@ -0,0 +1,11 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/mhaatha/go-template-saygenfix/internal/handler"
+)
+
+func TeacherRouter(handler handler.TeacherHandler, mux *http.ServeMux) {
+	mux.HandleFunc("GET /room-ujian-teacher", handler.RoomUjianView)
+}
